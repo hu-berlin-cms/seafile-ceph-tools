9ef8ff5474c6f84a42813f3942313f784b20d8f7
Enno Gröper <groepeen@cms.hu-berlin.de>
2015-09-22 17:03:31 +0200

    cleanup and PEP8

--- usage_statistics.py
+++ usage_statistics.py
@@ -7,6 +7,7 @@ import argparse
 
 from ctypes import c_char_p
 
+
 def ioctx_set_namespace(ioctx, namespace):
     '''Python rados client has no binding for rados_ioctx_set_namespace, we
     add it here.
@@ -55,10 +56,11 @@ class LibraryStatistics(object):
         self.objects['sum'] += 1
 
     def __str__(self):
-        #return ("uuid=%s,commits_size=%u,commits_obj=%u,fs_size=%u,fs_obj=%u,blocks_size=%u,blocks_obj=%u,size=%u,obj=%u"
         return ("%s,%u,%u,%u,%u,%u,%u,%u,%u"
-                % (self.uuid, self.sizes['commits'], self.objects['commits'], self.sizes['fs'], self.objects['fs'],
-                   self.sizes['blocks'], self.objects['blocks'], self.sizes['sum'], self.objects['sum']))
+                % (self.uuid, self.sizes['commits'], self.objects['commits'],
+                   self.sizes['fs'], self.objects['fs'],
+                   self.sizes['blocks'], self.objects['blocks'],
+                   self.sizes['sum'], self.objects['sum']))
 
 
 class IoCtxPool(object):
@@ -70,7 +72,8 @@ class IoCtxPool(object):
         self.conf = conf
         self.pool = Queue.Queue(pool_size)
         if conf.ceph_client_id:
-            self.cluster = rados.Rados(conffile=conf.ceph_conf_file, rados_id=conf.ceph_client_id)
+            self.cluster = rados.Rados(conffile=conf.ceph_conf_file,
+                                       rados_id=conf.ceph_client_id)
         else:
             self.cluster = rados.Rados(conffile=conf.ceph_conf_file)
 
@@ -97,6 +100,7 @@ class IoCtxPool(object):
         except Queue.Full:
             ioctx.close()
 
+
 class SeafCephClient(object):
     '''Wraps a Ceph ioctx'''
     def __init__(self, conf):
@@ -120,15 +124,19 @@ def main():
     cmd_parser = argparse.ArgumentParser(
         description="get seafile usage statistics from ceph"
     )
-    cmd_parser.add_argument("--client","--id","-i", dest="client", default='seafile-test',
-                            help="ceph client id")
-    cmd_parser.add_argument("--blocks-pool","-b", dest="blocks", default='seafile-test-blocks',
+    cmd_parser.add_argument("--client", "--id", "-i", dest="client",
+                            default='seafile', help="ceph client id")
+    cmd_parser.add_argument("--blocks-pool", "-b", dest="blocks",
+                            default='seafile-blocks',
                             help="ceph pool containing block objects")
-    cmd_parser.add_argument("--commits-pool","-c", dest="commits", default='seafile-test-commits',
+    cmd_parser.add_argument("--commits-pool", "-c", dest="commits",
+                            default='seafile-commits',
                             help="ceph pool containing commit objects")
-    cmd_parser.add_argument("--fs-pool","-f", dest="fs", default='seafile-test-commits',
+    cmd_parser.add_argument("--fs-pool", "-f", dest="fs",
+                            default='seafile-commits',
                             help="ceph pool containing fs objects")
-    cmd_parser.add_argument("--config", dest="config", default='/etc/ceph/ceph.conf',
+    cmd_parser.add_argument("--config", dest="config",
+                            default='/etc/ceph/ceph.conf',
                             help="ceph.conf")
     cmd_parser.add_argument('-V', '--verbose', action="store_true",
                             dest="verbose",
@@ -141,23 +149,28 @@ def main():
     pool = IoCtxPool(conf)
 
     # get objects of all namespaces for every pool
-    for objtype, poolname in {'commits': args.commits, 'fs': args.fs, 'blocks': args.blocks}.items():
+    ceph_pools = {'commits': args.commits,
+                  'fs': args.fs, 'blocks': args.blocks}
+
+    for objtype, poolname in ceph_pools.items():
         pool.conf.pool_name = poolname
         ioctx = pool.create_ioctx()
         ioctx_set_namespace(ioctx, '\001')
         for obj in ioctx.list_objects():
             if not obj.nspace in libraries:
                 libraries[obj.nspace] = LibraryStatistics(obj.nspace)
-            
+
             ioctx_set_namespace(ioctx, obj.nspace)
             libraries[obj.nspace].add_obj(objtype, ioctx.stat(obj.key)[0])
         ioctx.close()
-        
+
     # output results
-    print "uuid,commits_size,commits_obj,fs_size,fs_obj,blocks_size,blocks_obj,size,obj"
+    print "uuid,commits_size,commits_obj,fs_size,fs_obj,blocks_size,"\
+          "blocks_obj,size,obj"
+
     for lib in libraries.values():
         print lib
- 
+
 
 if __name__ == '__main__':
     main()
